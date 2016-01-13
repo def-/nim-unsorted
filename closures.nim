b85c6b4158d3c071c4d5958111714e3412043843
def <dennis@felsin9.de>
2016-01-13 14:59:08 +0100

    Fix closures with new-ll

--- closures.nim
+++ closures.nim
@@ -1,8 +1,9 @@
 var funcs: seq[proc(): int] = @[]
 
 for i in 0..9:
-  let x = i
-  funcs.add(proc (): int = x * x)
+  (proc =
+    let x = i
+    funcs.add(proc (): int = x * x))()
 
 for i in 0..8:
   echo "func[", i, "]: ", funcs[i]()
