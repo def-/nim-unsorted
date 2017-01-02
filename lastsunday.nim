c87e3f48586420b67f6edf8d2581af3d466160bd
Dennis Felsing <dennis@felsin9.de>
2017-01-02 10:20:28 +0100

    Fix lastfriday/lastsunday

--- lastsunday.nim
+++ lastsunday.nim
@@ -4,7 +4,7 @@ let year = paramStr(1).parseInt
 for month in mJan .. mDec:
   for day in countdown(getDaysInMonth(month, year), 1):
     let t = getGMTime toTime TimeInfo(second: 0, minute: 0, hour: 12,
-      monthday: day, month: month, year: year, tzname: "GMT")
+      monthday: day, month: month, year: year, timezone: 0)
     if t.weekday == dSun:
       echo t.format "yyyy-MM-dd"
       break
